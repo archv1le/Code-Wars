8a0aac580a49ddd8c2ffbd01da6207e19721e583
archv1le <158765690+archv1le@users.noreply.github.com>
2024-02-05 11:20:46 +0600

    Create String ends with?.cpp

---
+++ C++/7-kyu/String ends with?.cpp
@@ -0,0 +1,9 @@
+#include <string>
+
+bool solution(std::string const &str, std::string const &ending) {
+  if (str.length() < ending.length()) {
+    return false;
+  }
+  
+  return str.compare(str.length() - ending.length(), ending.length(), ending) == 0;
+}
