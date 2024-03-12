c49da6239309873a14339a2d251c6f1ac6446907
archv1le <158765690+archv1le@users.noreply.github.com>
2024-03-13 00:46:21 +0600

    Create  Your order, please.py

---
+++ Python/6-kyu/Your order, please.py
@@ -0,0 +1,7 @@
+def order(sentence):
+    if not sentence:
+        return ""
+
+    words = sentence.split()
+    sorted_words = sorted(words, key=lambda x: int(''.join(filter(str.isdigit, x))))
+    return ' '.join(sorted_words)
