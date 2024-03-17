e73f9ac3a8c0ff197aba9970da3659422c430fd3
archv1le <158765690+archv1le@users.noreply.github.com>
2024-03-17 21:55:19 +0600

    Create Sudoku Solver.py

---
+++ Python/3-kyu/Sudoku Solver.py
@@ -0,0 +1,32 @@
+def is_valid_move(board, row, col, num):
+    if num in board[row]:
+        return False
+    
+    if num in [board[i][col] for i in range(9)]:
+        return False
+    
+    start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+    for i in range(start_row, start_row + 3):
+        for j in range(start_col, start_col + 3):
+            if board[i][j] == num:
+                return False
+    
+    return True
+
+def solve_sudoku(board):
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0:
+                for num in range(1, 10):
+                    if is_valid_move(board, row, col, num):
+                        board[row][col] = num
+                        if solve_sudoku(board):
+                            return True
+                        board[row][col] = 0
+                return False
+    return True
+
+def sudoku(puzzle):
+    board = [row[:] for row in puzzle]
+    solve_sudoku(board)
+    return board
