4aaafc43d342514841556bcd7e36a12582817b03
archv1le <158765690+archv1le@users.noreply.github.com>
2024-03-24 21:41:46 +0600

    Create Mexican Wave.py

---
+++ Python/6-kyu/Mexican Wave.py
@@ -0,0 +1,6 @@
+def wave(people):
+    result = []
+    for i in range(len(people)):
+        if people[i].isalpha():
+            result.append(people[:i] + people[i].upper() + people[i+1:])
+    return result
